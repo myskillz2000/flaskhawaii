7e2830a7e18bc47769922a01c6f8e171f31fdb1d
myskillz2000 <matthewstiller484@yahoo.com>
2021-08-25 17:40:32 -0400

    Created the compose file to make the flask application accesible in a container.

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,3 @@ RUN mkdir /app
 ADD . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello from Hawaii!</h1>"
 
+@app.route('/info')
+def info():
+    return "<h1>This was our honeymoon.</h1>"
+
+@app.route('/some_page/<name>')
+def some_page(name):
+    return "<h1>This is an example of dynamic route{}</h1>".format(name)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
