7eb62d3c870b9bca3790700bd46bf4991e5e0c82
myskillz2000 <matthewstiller484@yahoo.com>
2021-08-25 22:34:20 -0400

    Updated list items into Hawaiian locations.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def index():
     mylist = ['Lihue', 'Waimea', 'Princeville', 'Napali Coast',
               'Smith Family Luau', 'Pearl Harbor/Polynesian Cultural Center',
               'Fern Grotto', 'Bali Hai Resort']
-    return render_template('basic.html', mylist=mylist)
+    return render_template('basic.html', locations=locations)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
