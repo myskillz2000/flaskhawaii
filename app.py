45f93634e3206cffb45d9a7ca57f8198263a1a06
myskillz2000 <matthewstiller484@yahoo.com>
2021-08-25 22:29:01 -0400

    Created the locations in which I would like to add my pictures too as mylist.

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    mylist = ['Lihue', 'Waimea', 'Princeville', 'Napali Coast',
+              'Smith Family Luau', 'Pearl Harbor/Polynesian Cultural Center',
+              'Fern Grotto', 'Bali Hai Resort']
+    return render_template('basic.html', mylist=mylist)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
