1e08929df1aee4eea1095843d01b0f0d1dd39674
myskillz2000 <matthewstiller484@yahoo.com>
2021-08-25 21:57:11 -0400

    Made it follow a simple pattern with limited information.  I added the static folder now too.

--- app.py
+++ app.py
@@ -5,13 +5,5 @@ app = Flask(__name__)
 def index():
     return render_template('basic.html')
 
-@app.route('/info')
-def info():
-    return "<h1>This was our honeymoon.</h1>"
-
-@app.route('/some_page/<name>')
-def some_page(name):
-    return "<h1>This is an example of dynamic route{}</h1>".format(name)
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
